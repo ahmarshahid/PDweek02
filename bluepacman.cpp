39e6b1e8bba70604583b385fd56ed000807ca3d5
ahmarjan456@gmail.com <ahmarjan456@gmail.com>
2022-12-18 13:26:29 +0500

    Iam uploading my files of PDweek-02

---
+++ bluepacman.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int main(){
+
+system("color 97");
+
+cout << "                            .::---::..                                                     "<<endl;
+cout << "                         .-------------.                                                   "<<endl;
+cout << "                       .------------------.                                                 "<<endl;
+cout << "                      .-----------------:.                                                                    "<<endl;
+cout << "                      :------------::.                                                                     "<<endl;
+cout << "                      ------------:.                                                                     "<<endl;
+cout << "                      :--------------:..                                                                     "<<endl;
+cout << "                       :-----------------:.                                                                    "<<endl;
+cout << "                        .----------------:                                                                   "<<endl;
+cout << "                         .:-----------:.                                                                  "<<endl;
+cout << "                            .::---::..                                                               "<<endl;
+cout << "                                                                                           "<<endl;
+cout << "                                                                                           "<<endl;
+cout << "                                                                                           "<<endl;
+cout << "                                                                                           "<<endl;
+cout << "                                                                                           "<<endl;
+cout << "                                                                                           "<<endl;
+cout << "                                                                                           "<<endl;
+cout << "                                                                                           "<<endl;
+cout << "                                                                                           "<<endl;
+cout << "                                                                                           "<<endl;
+ 
+return 0;
+
+}
---
+++ car.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+main(){
+cout << "                                                                                        "<<endl;
+cout << "                           _______________________________                                                                   "<<endl;
+cout << "                          /                |              \\                              "<<endl;
+cout << "                         /                 |               \\                              "<<endl;
+cout << "              __________/__________________|________________\\_____________                             "<<endl;
+cout << "             / o                         00   |    00                    o|              "<<endl;
+cout << "            /                                 |                           |              "<<endl;
+cout << "           / o      _______                   |          _______         o|             "<<endl;
+cout << "           |_______/   o   \\________________|___________/   o   \\_________|              "<<endl;
+cout << "                   \\_______/                            \\_______/                               "<<endl;
+cout << "                                                                                        "<<endl;
+cout << "                                                                                        "<<endl;
+cout << "                                                                                        "<<endl;
+cout << "                                                                                        "<<endl;
+cout << "                                                                                        "<<endl;
+cout << "                                                                                        "<<endl;
+cout << "                                                                                        "<<endl;
+cout << "                                                                                        "<<endl;
+cout << "                                                                                        "<<endl;
+cout << "                                                                                        "<<endl;
+cout << "                                                                                        "<<endl;
+cout << "                                                                                        "<<endl;
+cout << "                                                                                        "<<endl;
+cout << "                                                                                        "<<endl;
+cout << "                                                                                        "<<endl;
+
+}
+
+
+
+
+
+
+
+
+
+
+
